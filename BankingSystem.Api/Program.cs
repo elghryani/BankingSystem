760868343dce806a19cbe99a78943b919476e58f
Elghryani Muntaser <Elghryani.muntaser@gmail.com>
2026-09-21 11:20:59 +0200

    feat: Application-configure

--- BankingSystem.Api/Program.cs
+++ BankingSystem.Api/Program.cs
@@ -1,8 +1,12 @@
+using BankingSystem.Application;
 using BankingSystem.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
 
 var app = builder.Build();
 

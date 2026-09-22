792d5e1506b183a456bf8d256157f09ba3b7c07f
Elghryani Muntaser <Elghryani.muntaser@gmail.com>
2026-09-22 11:01:13 +0200

    feat: create kycProfile-controller

---
+++ BankingSystem.Api/Controllers/KycProfilesController.cs
@@ -0,0 +1,24 @@
+﻿using BankingSystem.Application.Features.KycProfiles.Commands.CreateKycProfile;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KycProfilesController : ControllerBase
+    {
+        private readonly ISender _sender;
+        public KycProfilesController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateKycProfileCommand command,CancellationToken cancellationToken)
+        {
+            var kycProfileId = await _sender.Send(command, cancellationToken);
+            return Ok(kycProfileId);
+        }
+    }
+}
--- BankingSystem.Domain/Entities/Customer.cs
+++ BankingSystem.Domain/Entities/Customer.cs
@@ -1,77 +1,77 @@
-﻿using BankingSystem.Domain.Enums;
+﻿    using BankingSystem.Domain.Enums;
 
-namespace BankingSystem.Domain.Entities
-{
-    public sealed class Customer
+    namespace BankingSystem.Domain.Entities
     {
-        public Guid Id { get;private set; }
-        public string FirstName { get; private set; } = null!;
-        public string LastName { get;private set; } = null!;
-        public string? Email { get;private set; }
-        public string PhoneNumber { get; private set; } = null!; 
-        public EnUserStatus Status { get;private set; }
-        public DateTime CreatedAt { get;private set; }
-        public DateTime? UpdatedAt { get;private set; }
-
-        public KycProfile? Profile { get; private set; }
-        public ICollection<Account> Accounts { get; private set; } = new List<Account>();
-        private Customer() { }
-        public static Customer Create(string firstName,string lastName, string? email,string phoneNumber)
+        public sealed class Customer
         {
-            return new Customer()
+            public Guid Id { get;private set; }
+            public string FirstName { get; private set; } = null!;
+            public string LastName { get;private set; } = null!;
+            public string? Email { get;private set; }
+            public string PhoneNumber { get; private set; } = null!; 
+            public EnUserStatus Status { get;private set; }
+            public DateTime CreatedAt { get;private set; }
+            public DateTime? UpdatedAt { get;private set; }
+
+            public KycProfile? Profile { get; private set; }
+            public ICollection<Account> Accounts { get; private set; } = new List<Account>();
+            private Customer() { }
+            public static Customer Create(string firstName,string lastName, string? email,string phoneNumber)
             {
-                Id = Guid.NewGuid(),
-                FirstName = firstName,
-                LastName = lastName,
-                Email = email,
-                PhoneNumber = phoneNumber,
-                Status = EnUserStatus.Pending,
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = null
-            };
-        }
+                return new Customer()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    PhoneNumber = phoneNumber,
+                    Status = EnUserStatus.Pending,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = null
+                };
+            }
 
-        public void AddKycProfile(string identityNumber, DateOnly dateOfBirth, string address, EnSourceOfIncome sourceOfIncome)
-        {
-            if (Profile != null)
-                throw new InvalidOperationException("Customer already has a KYC profile.");
+            public void AddKycProfile(string identityNumber, DateOnly dateOfBirth, string address, EnSourceOfIncome sourceOfIncome)
+            {
+                if (Profile != null)
+                    throw new InvalidOperationException("Customer already has a KYC profile.");
 
-            Profile = KycProfile.Create(Id,identityNumber,dateOfBirth,address,sourceOfIncome);
-        }
+                Profile = KycProfile.Create(Id,identityNumber,dateOfBirth,address,sourceOfIncome);
+            }
 
-        public void UpdateKycProfile(string identityNumber, DateOnly dateOfBirth, string address, EnSourceOfIncome sourceOfIncome)
-        {
-            if (Profile == null)
-                throw new InvalidOperationException("Customer does not have a KYC profile.");
+            public void UpdateKycProfile(string identityNumber, DateOnly dateOfBirth, string address, EnSourceOfIncome sourceOfIncome)
+            {
+                if (Profile == null)
+                    throw new InvalidOperationException("Customer does not have a KYC profile.");
 
-            Profile.Update(identityNumber, dateOfBirth, address, sourceOfIncome);
-        }
-        public void Update(string firstName, string lastName, string? email, string phoneNumber)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            PhoneNumber = phoneNumber;
-            UpdatedAt = DateTime.UtcNow;
-        }
+                Profile.Update(identityNumber, dateOfBirth, address, sourceOfIncome);
+            }
+            public void Update(string firstName, string lastName, string? email, string phoneNumber)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Email = email;
+                PhoneNumber = phoneNumber;
+                UpdatedAt = DateTime.UtcNow;
+            }
 
-        public void Active()
-        {
-            if (Profile == null)
-                throw new InvalidOperationException("KYC profile is required.");
+            public void Active()
+            {
+                if (Profile == null)
+                    throw new InvalidOperationException("KYC profile is required.");
 
-            if (Status != EnUserStatus.Pending)
-                throw new InvalidOperationException("User must be pending.");
+                if (Status != EnUserStatus.Pending)
+                    throw new InvalidOperationException("User must be pending.");
 
-            Profile.VerifyKyc();
+                Profile.VerifyKyc();
 
-            Status = EnUserStatus.Active;
+                Status = EnUserStatus.Active;
 
-        }
-        public void Deactivate()
-        {
-            Status = EnUserStatus.InActive;
-            UpdatedAt = DateTime.UtcNow;
+            }
+            public void Deactivate()
+            {
+                Status = EnUserStatus.InActive;
+                UpdatedAt = DateTime.UtcNow;
+            }
         }
     }
-}

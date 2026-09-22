50591cccd14ee7034c0c9f8b8c2bdeeb424e48cb
Elghryani Muntaser <Elghryani.muntaser@gmail.com>
2026-09-22 10:18:07 +0200

    feat: create customers-controller

---
+++ BankingSystem.Api/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using BankingSystem.Application.Features.Customers.Commands.ActivateCustomer;
+using BankingSystem.Application.Features.Customers.Commands.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public CustomersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCustomerCommand command,CancellationToken cancellationToken)
+        {
+            var customerId = await _sender.Send(command, cancellationToken);
+            return Ok(customerId);
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> ActivateCustomer(ActivateCustomerCommand command , CancellationToken cancellationToken)
+        {
+            var customer = await _sender.Send(command, cancellationToken);
+            return Ok(customer);
+        }
+      
+
+
+    }
+}
